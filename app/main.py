40af1d7bce1c6ae002916fd7f5640326ad2706c6
刘泽庆 <148669641+ximingzizi@users.noreply.github.com>
2026-04-09 14:08:28 +0800

    Add new CORS origin for production server

--- app/main.py
+++ app/main.py
@@ -26,7 +26,9 @@ app.add_middleware(
     CORSMiddleware,     # 允许跨域
     allow_origins=[
         "http://localhost:8080",        
-        "http://127.0.0.1:8080",], 
+        "http://127.0.0.1:8080",
+        "https://recommendserver-920l.onrender.com",
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
